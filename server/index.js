4b1d881fe2257c992d84bc526299d24a3ee4b5bd
Joel Noyce <noyce.joel@gmail.com>
2024-01-31 15:09:00 +0000

    updated render dist folder

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const cors = require('cors')
 var morgan = require("morgan");
 const app = express();
+app.use(express.static("dist"));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("build"));
+
 let persons = [
   {
     id: 1,
